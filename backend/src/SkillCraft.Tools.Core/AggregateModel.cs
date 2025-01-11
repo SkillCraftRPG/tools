b27b22fc05ca482a79010c464ce3ea1907066215
Francis Pion <francispion@hotmail.com>
2025-01-11 15:20:32 -0500

    core

---
+++ backend/src/SkillCraft.Tools.Core/AggregateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillCraft.Tools.Core;
+
+public abstract class AggregateModel
+{
+  public Guid Id { get; set; }
+
+  public override bool Equals(object? obj) => obj is AggregateModel aggregate && aggregate.Id == Id;
+  public override int GetHashCode() => Id.GetHashCode();
+  public override string ToString() => $"{GetType()} (Id={Id})";
+}
---
+++ backend/src/SkillCraft.Tools.Core/Specializations/ISpecializationQuerier.cs
@@ -0,0 +1,9 @@
+﻿using SkillCraft.Tools.Core.Specializations.Models;
+
+namespace SkillCraft.Tools.Core.Specializations;
+
+public interface ISpecializationQuerier
+{
+  Task<SpecializationModel?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
+  Task<SpecializationModel?> ReadAsync(string uniqueSlug, CancellationToken cancellationToken = default);
+}
