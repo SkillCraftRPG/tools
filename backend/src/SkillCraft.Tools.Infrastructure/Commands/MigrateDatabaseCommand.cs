b56437c9f0829c8f7cd202b2d2ce932837cdf2d1
Francis Pion <francispion@hotmail.com>
2025-01-11 19:49:37 -0500

    Implemented SQL Server persistence.

---
+++ backend/src/SkillCraft.Tools.Infrastructure/Commands/MigrateDatabaseCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace SkillCraft.Tools.Infrastructure.Commands;
+
+public record MigrateDatabaseCommand : IRequest;
+
+internal class MigrateDatabaseCommandHandler : IRequestHandler<MigrateDatabaseCommand>
+{
+  private readonly SkillCraftContext _context;
+
+  public MigrateDatabaseCommandHandler(SkillCraftContext context)
+  {
+    _context = context;
+  }
+
+  public async Task Handle(MigrateDatabaseCommand _, CancellationToken cancellationToken)
+  {
+    await _context.Database.MigrateAsync(cancellationToken);
+  }
+}
--- backend/src/SkillCraft.Tools/Program.cs
+++ backend/src/SkillCraft.Tools/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace SkillCraft.Tools;
+﻿using MediatR;
+using Microsoft.FeatureManagement;
+using SkillCraft.Tools.Infrastructure.Commands;
+
+namespace SkillCraft.Tools;
 
 internal class Program
 {
@@ -12,6 +16,14 @@ internal class Program
     WebApplication application = builder.Build();
 
     await startup.ConfigureAsync(application);
+
+    IFeatureManager featureManager = application.Services.GetRequiredService<IFeatureManager>();
+    if (await featureManager.IsEnabledAsync(FeatureFlags.MigrateDatabase))
+    {
+      using IServiceScope scope = application.Services.CreateScope();
+      IMediator mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+      await mediator.Send(new MigrateDatabaseCommand());
+    }
 
     application.Run();
   }
