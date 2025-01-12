9362b69b7c19f7e3f5beb8e89733086a531b1ac3
Francis Pion <francispion@hotmail.com>
2025-01-11 23:09:30 -0500

    ActorModel

--- backend/src/SkillCraft.Tools.Core/Actors/Models/ActorModel.cs
+++ backend/src/SkillCraft.Tools.Core/Actors/Models/ActorModel.cs
@@ -9,4 +9,18 @@ public class ActorModel
   public string DisplayName { get; set; } = "System";
   public string? EmailAddress { get; set; }
   public string? PictureUrl { get; set; }
+
+  public override bool Equals(object? obj) => obj is ActorModel actor && actor.Id == Id;
+  public override int GetHashCode() => Id.GetHashCode();
+  public override string ToString()
+  {
+    StringBuilder actor = new();
+    actor.Append(DisplayName);
+    if (EmailAddress != null)
+    {
+      actor.Append(" <").Append(EmailAddress).Append('>');
+    }
+    actor.Append(" (").Append(Type).Append(".Id=").Append(Id).Append(')');
+    return actor.ToString();
+  }
 }
