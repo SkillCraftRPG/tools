badf9f6f5e2f03218f09dcf01f526b5d53c48200
Francis Pion <francispion@hotmail.com>
2025-01-11 23:10:26 -0500

    worker

---
+++ backend/src/SkillCraft.Tools.Worker/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Logitar.Portal.Client;
+using SkillCraft.Tools.Core;
+using SkillCraft.Tools.Infrastructure;
+using SkillCraft.Tools.Infrastructure.SqlServer;
+using SkillCraft.Tools.Worker.Portal;
+
+namespace SkillCraft.Tools.Worker;
+
+internal class Startup
+{
+  private readonly IConfiguration _configuration;
+
+  public Startup(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
+
+  public void ConfigureServices(IServiceCollection services)
+  {
+    IPortalSettings portalSettings = _configuration.GetSection(PortalSettings.SectionKey).Get<PortalSettings>() ?? new();
+    portalSettings = WorkerPortalSettings.Initialize(portalSettings);
+    services.AddLogitarPortalClient(portalSettings);
+
+    services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+    services.AddHostedService<SeedingWorker>();
+
+    services.AddSkillCraftToolsCore();
+    services.AddSkillCraftToolsInfrastructure();
+    DatabaseProvider databaseProvider = _configuration.GetValue<DatabaseProvider?>("DatabaseProvider") ?? DatabaseProvider.SqlServer;
+    switch (databaseProvider)
+    {
+      case DatabaseProvider.SqlServer:
+        services.AddSkillCraftToolsInfrastructureSqlServer(_configuration);
+        break;
+      default:
+        throw new DatabaseProviderNotSupportedException(databaseProvider);
+    }
+  }
+}
