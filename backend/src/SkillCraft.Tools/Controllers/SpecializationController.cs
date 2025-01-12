7b5d81f5a0f3b6c30d807bc58aa4f7e24fd45855
Francis Pion <francispion@hotmail.com>
2025-01-11 20:15:55 -0500

    Added controllers and OpenAPI with Scalar UI. (#11)

---
+++ backend/src/SkillCraft.Tools/Controllers/SpecializationController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SkillCraft.Tools.Core.Specializations.Models;
+using SkillCraft.Tools.Core.Specializations.Queries;
+
+namespace SkillCraft.Tools.Controllers;
+
+[ApiController]
+//[Authorize] // ISSUE: https://github.com/SkillCraftRPG/tools/issues/5
+[Route("api/specializations")]
+public class SpecializationController : ControllerBase
+{
+  private readonly IMediator _mediator;
+
+  public SpecializationController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<SpecializationModel>> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    ReadSpecializationQuery query = new(id, Slug: null);
+    SpecializationModel? specialization = await _mediator.Send(query, cancellationToken);
+    return specialization == null ? NotFound() : Ok(specialization);
+  }
+
+  [HttpGet("slug:{slug}")]
+  public async Task<ActionResult<SpecializationModel>> ReadAsync(string slug, CancellationToken cancellationToken)
+  {
+    ReadSpecializationQuery query = new(Id: null, slug);
+    SpecializationModel? specialization = await _mediator.Send(query, cancellationToken);
+    return specialization == null ? NotFound() : Ok(specialization);
+  }
+}
--- backend/src/SkillCraft.Tools/FeatureFlags.cs
+++ backend/src/SkillCraft.Tools/FeatureFlags.cs
@@ -5,4 +5,5 @@ internal static class FeatureFlags
   public const string MigrateDatabase = "MigrateDatabase";
   public const string UseGraphQLGraphiQL = "UseGraphQLGraphiQL";
   public const string UseGraphQLVoyager = "UseGraphQLVoyager";
+  public const string UseScalarUI = "UseScalarUI";
 }
