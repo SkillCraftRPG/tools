f97b043908b06068018a765de564a48e015b02a3
Francis Pion <francispion@hotmail.com>
2025-01-13 13:35:21 -0500

    GraphQL

---
+++ backend/src/SkillCraft.Tools/GraphQL/Languages/SearchLanguagesPayloadGraphType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using SkillCraft.Tools.Core.Languages.Models;
+using SkillCraft.Tools.GraphQL.Search;
+
+namespace SkillCraft.Tools.GraphQL.Languages;
+
+internal class SearchLanguagesPayloadGraphType : SearchPayloadInputGraphType<SearchLanguagesPayload>
+{
+  public SearchLanguagesPayloadGraphType() : base()
+  {
+    Field(x => x.Script)
+      .Description("When specified, only languages written using this script will match.");
+
+    Field(x => x.Sort, type: typeof(NonNullGraphType<ListGraphType<NonNullGraphType<LanguageSortOptionGraphType>>>))
+      .DefaultValue([])
+      .Description("The sort parameters of the search.");
+  }
+}
