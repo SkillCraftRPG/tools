306df2a0aff2f0737eb01e4a9914e23f8a3f3194
Francis Pion <francispion@hotmail.com>
2025-01-12 20:20:10 -0500

    RootQuery

--- backend/src/SkillCraft.Tools/GraphQL/RootQuery.cs
+++ backend/src/SkillCraft.Tools/GraphQL/RootQuery.cs
@@ -1,5 +1,6 @@
 ﻿using GraphQL.Types;
 using SkillCraft.Tools.GraphQL.Customizations;
+using SkillCraft.Tools.GraphQL.Educations;
 using SkillCraft.Tools.GraphQL.Natures;
 using SkillCraft.Tools.GraphQL.Specializations;
 using SkillCraft.Tools.GraphQL.Talents;
@@ -13,6 +14,7 @@ internal class RootQuery : ObjectGraphType
     Name = "RootQuery";
 
     CustomizationQueries.Register(this);
+    EducationQueries.Register(this);
     NatureQueries.Register(this);
     SpecializationQueries.Register(this);
     TalentQueries.Register(this);
