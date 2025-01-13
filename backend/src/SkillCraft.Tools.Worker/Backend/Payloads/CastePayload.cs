b26825a7e88bb5efb573e69045d1b7bc516f5c8d
Francis Pion <francispion@hotmail.com>
2025-01-12 21:34:49 -0500

    seed

---
+++ backend/src/SkillCraft.Tools.Worker/Backend/Payloads/CastePayload.cs
@@ -0,0 +1,8 @@
+﻿using SkillCraft.Tools.Core.Castes.Models;
+
+namespace SkillCraft.Tools.Worker.Backend.Payloads;
+
+internal record CastePayload : CreateOrReplaceCastePayload
+{
+  public Guid Id { get; set; }
+}
