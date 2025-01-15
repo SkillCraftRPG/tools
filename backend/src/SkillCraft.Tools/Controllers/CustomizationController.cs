6ae8b8cdc3b1fde047fa130c2670914879b02b0d
Francis Pion <francispion@hotmail.com>
2025-01-14 23:47:21 -0500

    Implemented Customization Views. (#43)

--- backend/src/SkillCraft.Tools/Controllers/CustomizationController.cs
+++ backend/src/SkillCraft.Tools/Controllers/CustomizationController.cs
@@ -1,19 +1,13 @@
 ﻿using Logitar.Portal.Contracts.Search;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SkillCraft.Tools.Constants;
-using SkillCraft.Tools.Core.Customizations.Commands;
 using SkillCraft.Tools.Core.Customizations.Models;
 using SkillCraft.Tools.Core.Customizations.Queries;
-using SkillCraft.Tools.Models.Customization;
 
 namespace SkillCraft.Tools.Controllers;
 
-[ApiController]
-[Authorize(Policy = Policies.IsAdmin)]
-[Route("api/customizations")]
-public class CustomizationController : ControllerBase
+[Route("dons-handicaps")]
+public class CustomizationController : Controller
 {
   private readonly IMediator _mediator;
 
@@ -22,59 +16,27 @@ public class CustomizationController : ControllerBase
     _mediator = mediator;
   }
 
-  [HttpPost]
-  public async Task<ActionResult<CustomizationModel>> CreateAsync([FromBody] CreateOrReplaceCustomizationPayload payload, CancellationToken cancellationToken)
-  {
-    CreateOrReplaceCustomizationCommand command = new(Id: null, payload, Version: null);
-    CreateOrReplaceCustomizationResult result = await _mediator.Send(command, cancellationToken);
-    return ToActionResult(result);
-  }
-
-  [HttpGet("{id}")]
-  public async Task<ActionResult<CustomizationModel>> ReadAsync(Guid id, CancellationToken cancellationToken)
-  {
-    ReadCustomizationQuery query = new(id, Slug: null);
-    CustomizationModel? customization = await _mediator.Send(query, cancellationToken);
-    return customization == null ? NotFound() : Ok(customization);
-  }
-
-  [HttpGet("slug:{slug}")]
-  public async Task<ActionResult<CustomizationModel>> ReadAsync(string slug, CancellationToken cancellationToken)
-  {
-    ReadCustomizationQuery query = new(Id: null, slug);
-    CustomizationModel? customization = await _mediator.Send(query, cancellationToken);
-    return customization == null ? NotFound() : Ok(customization);
-  }
-
-  [HttpPut("{id}")]
-  public async Task<ActionResult<CustomizationModel>> ReplaceAsync(Guid id, [FromBody] CreateOrReplaceCustomizationPayload payload, long? version, CancellationToken cancellationToken)
-  {
-    CreateOrReplaceCustomizationCommand command = new(id, payload, version);
-    CreateOrReplaceCustomizationResult result = await _mediator.Send(command, cancellationToken);
-    return ToActionResult(result);
-  }
-
   [HttpGet]
-  public async Task<ActionResult<SearchResults<CustomizationModel>>> SearchAsync([FromQuery] SearchCustomizationsParameters parameters, CancellationToken cancellationToken)
+  public async Task<ActionResult> CustomizationList(CancellationToken cancellationToken)
   {
-    SearchCustomizationsQuery query = new(parameters.ToPayload());
+    SearchCustomizationsPayload payload = new();
+    SearchCustomizationsQuery query = new(payload);
     SearchResults<CustomizationModel> customizations = await _mediator.Send(query, cancellationToken);
-    return Ok(customizations);
+
+    return View(customizations);
   }
 
-  private ActionResult<CustomizationModel> ToActionResult(CreateOrReplaceCustomizationResult result)
+  [HttpGet("{idOrSlug}")]
+  public async Task<ActionResult> CustomizationView(string idOrSlug, CancellationToken cancellationToken)
   {
-    CustomizationModel? customization = result.Customization;
+    bool parsed = Guid.TryParse(idOrSlug, out Guid id);
+    ReadCustomizationQuery query = new(parsed ? id : null, idOrSlug);
+    CustomizationModel? customization = await _mediator.Send(query, cancellationToken);
     if (customization == null)
     {
       return NotFound();
     }
-    else if (!result.Created)
-    {
-      return Ok(customization);
-    }
 
-    Uri uri = new($"{Request.Scheme}://{Request.Host}/api/customizations/{customization.Id}", UriKind.Absolute);
-    return Created(uri, customization);
+    return View(customization);
   }
 }
