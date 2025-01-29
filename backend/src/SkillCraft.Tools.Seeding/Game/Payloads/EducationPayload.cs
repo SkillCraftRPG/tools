327f6b35603062b148612dae29a01b88d0d665fc
Francis Pion <francispion@hotmail.com>
2025-01-29 14:37:50 -0500

    Seed Educations.

---
+++ backend/src/SkillCraft.Tools.Seeding/Game/Payloads/EducationPayload.cs
@@ -0,0 +1,13 @@
+﻿namespace SkillCraft.Tools.Seeding.Game.Payloads;
+
+public record EducationPayload
+{
+  public Guid Id { get; set; }
+
+  public string UniqueSlug { get; set; } = string.Empty;
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+
+  public Skill? Skill { get; set; }
+  public double? WealthMultiplier { get; set; }
+}
---
+++ backend/src/SkillCraft.Tools.Seeding/Game/Tasks/SeedEducationsTask.cs
@@ -0,0 +1,107 @@
+﻿using Logitar.Cms.Core.Contents;
+using Logitar.Cms.Core.Contents.Commands;
+using Logitar.Cms.Core.Contents.Models;
+using Logitar.Cms.Core.Localization.Models;
+using MediatR;
+using SkillCraft.Tools.Core.Contents;
+using SkillCraft.Tools.Seeding.Game.Payloads;
+
+namespace SkillCraft.Tools.Seeding.Game.Tasks;
+
+internal class SeedEducationsTask : SeedingTask
+{
+  public override string? Description => "Seeds the educations into the CMS.";
+
+  public LanguageModel Language { get; }
+
+  public SeedEducationsTask(LanguageModel language)
+  {
+    Language = language;
+  }
+}
+
+internal class SeedEducationsTaskHandler : INotificationHandler<SeedEducationsTask>
+{
+  private readonly IContentTypeQuerier _contentTypeQuerier;
+  private readonly ILogger<SeedEducationsTaskHandler> _logger;
+  private readonly IMediator _mediator;
+
+  public SeedEducationsTaskHandler(IContentTypeQuerier contentTypeQuerier, ILogger<SeedEducationsTaskHandler> logger, IMediator mediator)
+  {
+    _contentTypeQuerier = contentTypeQuerier;
+    _logger = logger;
+    _mediator = mediator;
+  }
+
+  public async Task Handle(SeedEducationsTask task, CancellationToken cancellationToken)
+  {
+    string json = await File.ReadAllTextAsync("Game/data/educations.json", Encoding.UTF8, cancellationToken);
+    IEnumerable<EducationPayload>? educations = SeedingSerializer.Deserialize<IEnumerable<EducationPayload>>(json);
+    if (educations != null)
+    {
+      LanguageModel language = task.Language;
+      ContentTypeModel contentType = await _contentTypeQuerier.ReadAsync(Education.UniqueName, cancellationToken)
+        ?? throw new InvalidOperationException($"The content type '{Education.UniqueName}' could not be found.");
+      Dictionary<string, Guid> fields = contentType.Fields.ToDictionary(x => x.UniqueName, x => x.Id);
+
+      foreach (EducationPayload education in educations)
+      {
+        string displayText = education.DisplayName ?? education.UniqueSlug;
+
+        CreateOrReplaceContentPayload payload = new()
+        {
+          ContentTypeId = contentType.Id,
+          UniqueName = education.UniqueSlug,
+          DisplayName = education.DisplayName,
+          Description = education.Description
+        };
+        CreateOrReplaceContentCommand command = new(education.Id, language.Id, payload);
+        CreateOrReplaceContentResult result = await _mediator.Send(command, cancellationToken);
+        if (result.Content == null)
+        {
+          throw new InvalidOperationException($"'{nameof(CreateOrReplaceContentCommand)}' returned null for education 'Id={education.Id}' (locale).");
+        }
+        else if (result.Created)
+        {
+          _logger.LogInformation("The content locale ({Language}) was created for education '{Education}' (Id={Id}).", language.Locale, displayText, education.Id);
+        }
+        else
+        {
+          _logger.LogInformation("The content locale ({Language}) was updated for education '{Education}' (Id={Id}).", language.Locale, displayText, education.Id);
+        }
+
+        payload = new()
+        {
+          UniqueName = education.UniqueSlug
+        };
+        AddFieldValues(payload, fields, education);
+        command = new(education.Id, LanguageId: null, payload);
+        result = await _mediator.Send(command, cancellationToken);
+        if (result.Content == null)
+        {
+          throw new InvalidOperationException($"'{nameof(CreateOrReplaceContentCommand)}' returned null for education 'Id={education.Id}' (invariant).");
+        }
+        else if (result.Created)
+        {
+          _logger.LogInformation("The content locale invariant was created for education '{Education}' (Id={Id}).", displayText, education.Id);
+        }
+        else
+        {
+          _logger.LogInformation("The content locale invariant was updated for education '{Education}' (Id={Id}).", displayText, education.Id);
+        }
+      }
+    }
+  }
+
+  private static void AddFieldValues(CreateOrReplaceContentPayload payload, Dictionary<string, Guid> fields, EducationPayload education)
+  {
+    if (education.Skill.HasValue)
+    {
+      payload.AddFieldValue(fields[Education.Skill], education.Skill.Value);
+    }
+    if (education.WealthMultiplier.HasValue)
+    {
+      payload.AddFieldValue(fields[Education.WealthMultiplier], education.WealthMultiplier.Value);
+    }
+  }
+}
