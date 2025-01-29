79c003a96c19354fcddc7cd97071f959848d2a8d
Francis Pion <francispion@hotmail.com>
2025-01-29 14:39:42 -0500

    Seed Educations. (#78)

---
+++ backend/src/SkillCraft.Tools.Core/Contents/Education.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillCraft.Tools.Core.Contents;
+
+public static class Education
+{
+  public const string UniqueName = "Education";
+
+  public const string Skill = "Skill";
+  public const string WealthMultiplier = "WealthMultiplier";
+}
