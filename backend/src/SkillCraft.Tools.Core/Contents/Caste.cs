b77c896468c90e869d4603d70fefb48f54189179
Francis Pion <francispion@hotmail.com>
2025-01-29 15:25:29 -0500

    Seed Castes. (#80)

---
+++ backend/src/SkillCraft.Tools.Core/Contents/Caste.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillCraft.Tools.Core.Contents;
+
+public static class Caste
+{
+  public const string UniqueName = "Caste";
+
+  public const string Skill = "Skill";
+  public const string WealthRoll = "WealthRoll";
+  public const string Features = "Features";
+}
