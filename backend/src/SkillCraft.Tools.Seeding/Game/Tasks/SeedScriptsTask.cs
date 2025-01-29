ccc847001d44cdd2468c3970e9012639257529e7
Francis Pion <francispion@hotmail.com>
2025-01-29 14:14:54 -0500

    Seed Scripts & Languages.

---
+++ backend/src/SkillCraft.Tools.Seeding/Game/Tasks/SeedScriptsTask.cs
@@ -0,0 +1,94 @@
+﻿using Logitar.Cms.Core.Contents;
+using Logitar.Cms.Core.Contents.Commands;
+using Logitar.Cms.Core.Contents.Models;
+using Logitar.Cms.Core.Localization.Models;
+using MediatR;
+using SkillCraft.Tools.Core.Contents;
+using SkillCraft.Tools.Seeding.Game.Payloads;
+
+namespace SkillCraft.Tools.Seeding.Game.Tasks;
+
+internal class SeedScriptsTask : SeedingTask
+{
+  public override string? Description => "Seeds the scripts into the CMS.";
+
+  public LanguageModel Language { get; }
+
+  public SeedScriptsTask(LanguageModel language)
+  {
+    Language = language;
+  }
+}
+
+internal class SeedScriptsTaskHandler : INotificationHandler<SeedScriptsTask>
+{
+  private readonly IContentTypeQuerier _contentTypeQuerier;
+  private readonly ILogger<SeedScriptsTaskHandler> _logger;
+  private readonly IMediator _mediator;
+
+  public SeedScriptsTaskHandler(IContentTypeQuerier contentTypeQuerier, ILogger<SeedScriptsTaskHandler> logger, IMediator mediator)
+  {
+    _contentTypeQuerier = contentTypeQuerier;
+    _logger = logger;
+    _mediator = mediator;
+  }
+
+  public async Task Handle(SeedScriptsTask task, CancellationToken cancellationToken)
+  {
+    string json = await File.ReadAllTextAsync("Game/data/scripts.json", Encoding.UTF8, cancellationToken);
+    IEnumerable<ScriptPayload>? scripts = SeedingSerializer.Deserialize<IEnumerable<ScriptPayload>>(json);
+    if (scripts != null)
+    {
+      LanguageModel language = task.Language;
+      ContentTypeModel contentType = await _contentTypeQuerier.ReadAsync(Script.UniqueName, cancellationToken)
+        ?? throw new InvalidOperationException($"The content type '{Script.UniqueName}' could not be found.");
+      Dictionary<string, Guid> fields = contentType.Fields.ToDictionary(x => x.UniqueName, x => x.Id);
+
+      foreach (ScriptPayload script in scripts)
+      {
+        string displayText = script.DisplayName ?? script.UniqueSlug;
+
+        CreateOrReplaceContentPayload payload = new()
+        {
+          ContentTypeId = contentType.Id,
+          UniqueName = script.UniqueSlug,
+          DisplayName = script.DisplayName,
+          Description = script.Description
+        };
+        CreateOrReplaceContentCommand command = new(script.Id, language.Id, payload);
+        CreateOrReplaceContentResult result = await _mediator.Send(command, cancellationToken);
+        if (result.Content == null)
+        {
+          throw new InvalidOperationException($"'{nameof(CreateOrReplaceContentCommand)}' returned null for script 'Id={script.Id}' (locale).");
+        }
+        else if (result.Created)
+        {
+          _logger.LogInformation("The content locale ({Language}) was created for script '{Script}' (Id={Id}).", language.Locale, displayText, script.Id);
+        }
+        else
+        {
+          _logger.LogInformation("The content locale ({Language}) was updated for script '{Script}' (Id={Id}).", language.Locale, displayText, script.Id);
+        }
+
+        payload = new()
+        {
+          UniqueName = script.UniqueSlug
+        };
+        command = new(script.Id, LanguageId: null, payload);
+        result = await _mediator.Send(command, cancellationToken);
+        if (result.Content == null)
+        {
+          throw new InvalidOperationException($"'{nameof(CreateOrReplaceContentCommand)}' returned null for script 'Id={script.Id}' (invariant).");
+        }
+        else if (result.Created)
+        {
+          _logger.LogInformation("The content locale invariant was created for script '{Script}' (Id={Id}).", displayText, script.Id);
+        }
+        else
+        {
+          _logger.LogInformation("The content locale invariant was updated for script '{Script}' (Id={Id}).", displayText, script.Id);
+        }
+      }
+    }
+  }
+}
