1738ee3834ecd15226ff3e71a3b56db563245517
Francis Pion <francispion@hotmail.com>
2025-01-29 03:50:34 -0500

    Implemented Content Type & Field Type Seeding. (#73)

---
+++ backend/src/SkillCraft.Tools.Seeding/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Logitar.Cms.Core;
+using Logitar.Cms.Infrastructure;
+using Logitar.Cms.Infrastructure.PostgreSQL;
+using Logitar.Cms.Infrastructure.SqlServer;
+
+namespace SkillCraft.Tools.Seeding;
+
+internal class Startup
+{
+  private readonly IConfiguration _configuration;
+
+  public Startup(IConfiguration configuration)
+  {
+    _configuration = configuration;
+  }
+
+  public void ConfigureServices(IServiceCollection services)
+  {
+    services.AddHostedService<SeedingWorker>();
+    services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+    services.AddLogitarCmsCore();
+    services.AddLogitarCmsInfrastructure();
+
+    DatabaseProvider databaseProvider = _configuration.GetValue<DatabaseProvider?>("DatabaseProvider") ?? DatabaseProvider.SqlServer;
+    switch (databaseProvider)
+    {
+      case DatabaseProvider.PostgreSQL:
+        services.AddLogitarCmsWithPostgreSQL(_configuration);
+        break;
+      case DatabaseProvider.SqlServer:
+        services.AddLogitarCmsWithSqlServer(_configuration);
+        break;
+      default:
+        throw new DatabaseProviderNotSupportedException(databaseProvider);
+    }
+
+    services.AddSingleton<IApplicationContext, SeedingApplicationContext>();
+  }
+}
